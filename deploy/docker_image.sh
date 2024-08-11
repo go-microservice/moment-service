0f816250ea297e33a5d6d4e0e3b1fcb73e1596d2
Richard <quanlongwang@gmail.com>
2024-08-11 19:26:08 +0800

    Create docker_image.sh

---
+++ deploy/docker_image.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+tag=$1
+IMAGE_NAME="moment-service"
+NAMESPACE="go-microservices"
+REGISTRY="registry.cn-hangzhou.aliyuncs.com"
+
+if [[ -z "$tag" ]]; then
+  echo "tag is empty"
+  echo "usage: sh $0 <tag>"
+  exit 1
+fi
+
+# build image
+echo "1. build docker image"
+docker build -t ${REGISTRY}/${NAMESPACE}/${IMAGE_NAME}:${tag} -t ${REGISTRY}/${NAMESPACE}/${IMAGE_NAME}:latest -f deploy/docker/Dockerfile .
+
+# docker push new-repo:tagname
+echo "2. push docker image to remote hub"
+docker push ${REGISTRY}/${NAMESPACE}/${IMAGE_NAME}:${tag}
+
+echo "Done. push docker image success."
